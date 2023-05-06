e973fde8d985c062157891a4847bebd0e759a49f
javadabbasi22 <69572865+javadabbasi22@users.noreply.github.com>
2023-05-06 14:00:23 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`)
+})
