8e515ee8ce0b6c129dbdf979c92d3cc3b175a5d9
javadabbasi22 <69572865+javadabbasi22@users.noreply.github.com>
2023-05-07 10:26:34 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 //const router = jsonServer.router("db.json")
 const fs = require('fs')
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const db = JSON.parse(fs.readFileSync('db.json'))
 const router = jsonServer.router(db)
 
 const middlewares = jsonServer.defaults()
