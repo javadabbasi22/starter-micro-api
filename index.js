002236b22f11eb789f446faa6ca134565a13b226
javadabbasi22 <69572865+javadabbasi22@users.noreply.github.com>
2023-05-07 10:11:30 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router("db.json")
 var fs = require('fs');
-fs.writeFile("db.json", settings, { mode: 0666 }, next)
+fs.chmod("db.json",  0666, (err) => {
+  if (err) throw err;
+  console.log('The permissions for file "db.json" have been changed!');
+}); 
 
 const middlewares = jsonServer.defaults()
 
