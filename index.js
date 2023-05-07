e4586e35b96ea526cacbcce0654582dd401d4fb1
javadabbasi22 <69572865+javadabbasi22@users.noreply.github.com>
2023-05-07 10:06:23 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router("db.json")
 var fs = require('fs');
-fs.writeFile("db.json", (error) => {
-    //Since this code executes as root the file being created is read only.
-    //chmod() it
-    fs.chmod("db.json", 0666, (error) => {
-      console.log('Changed file permissions');
-    });
-});
+fs.writeFile("db.json", settings, { mode: 0666 }, next)
 
 const middlewares = jsonServer.defaults()
 
